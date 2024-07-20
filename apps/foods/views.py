c98d4f63b8c76aa5826753ddab69819048eab46c
Elvis Black <elvis.black.alpha@gmail.com>
2024-07-20 11:25:56 +0700

    dir for templates + test cases for base app

--- apps/foods/views.py
+++ apps/foods/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
+from django.http import JsonResponse
+import re
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1>THfood</h1>")
+    return render(request, 'foods/index.html')
