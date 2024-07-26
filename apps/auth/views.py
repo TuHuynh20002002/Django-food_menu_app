b006f30afd9ee159c6d350ee8f93c5c152043101
Tu Huynh <huynhlethanhtu@gmail.com>
2024-07-27 01:11:47 +0700

    add route + controller + view

--- apps/auth/views.py
+++ apps/auth/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
 
-# Create your views here.
+
+def login(request):
+    return render(request, 'auth/pages/login.html')
+
+
+def register(request):
+    return render(request, 'auth/pages/register.html')
