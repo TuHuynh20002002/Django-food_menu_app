d67d07473765cb3e0fa44bebe302491bc8d1c6bc
Tu Huynh <huynhlethanhtu@gmail.com>
2024-08-10 00:26:18 +0700

    add cart icon in navbar

--- THfood/settings.py
+++ THfood/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'fontawesomefree',
+
     'apps.base.apps.BaseConfig',
     'apps.foods.apps.FoodsConfig',
 ]
